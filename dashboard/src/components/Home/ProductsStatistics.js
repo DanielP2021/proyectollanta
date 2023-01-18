ec79d2e4ac728dd64b2047dc8ce5f479a1c55649
DanielP2021 <dspataron.fis@unach.edu.ec>
2023-01-18 09:15:44 -0500

    primer Commit

---
+++ dashboard/src/components/Home/ProductsStatistics.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ProductsStatistics = () => {
+  return (
+    <div className="col-xl-6 col-lg-12">
+      <div className="card mb-4 shadow-sm">
+        <article className="card-body">
+          <h5 className="card-title">Estadisticas de Productos</h5>
+          <iframe
+            style={{
+              background: "#FFFFFF",
+              border: "none",
+              borderRadius: "2px",
+              boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2);",
+              width: "100%",
+              height: "350px",
+            }}
+            src="https://charts.mongodb.com/charts-mern-tesis-ryals/embed/charts?id=62e412c8-0e3b-4839-8e16-f96fef546ee1&maxDataAge=3600&theme=light&autoRefresh=true"
+          ></iframe>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsStatistics;
---
+++ dashboard/src/components/Home/SalesStatistics.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const SaleStatistics = () => {
+  return (
+    <div className="col-xl-6 col-lg-12">
+      <div className="card mb-4 shadow-sm">
+        <article className="card-body">
+          <h5 className="card-title">Estadisticas de Ventas</h5>
+          <iframe
+            style={{
+              background: "#FFFFFF",
+              border: "none",
+              borderRadius: "2px",
+              boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2);",
+              width: "100%",
+              height: "350px",
+            }}
+            src="https://charts.mongodb.com/charts-mern-tesis-ryals/embed/charts?id=62e4117c-5305-4657-81c0-bf62e7fe9784&maxDataAge=3600&theme=light&autoRefresh=true"
+          ></iframe>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default SaleStatistics;
